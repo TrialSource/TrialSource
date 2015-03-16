a38c447c9c7163b421a002f863157d85d59da694
Ben Roaman <benroaman@gmail.com>
2015-03-16 19:12:26 -0400

    create doctor and create trial forms have fields, implemented datepickerJS

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,30 +1,27 @@
 app.router.add('', function() {
   $('#main-content').html($('#landing-page').html()); // insert landing-page into main-content
 
-  $('.lp-search-button').click(function(e) {
+  $('.search-form').submit(function(e) {
     e.preventDefault();
-    alert('search!');
     document.location.hash = 'search/' + $('.condition-field').val();
   });
 
-  $('.lp-login-button').click(function(e) {
+  $('.login-form').submit(function(e) {
     e.preventDefault();
-    alert('login!');
-    document.location.hash = 'institution/researchers/' + $('.un-field').val();
+    document.location.hash = 'researcher/create/' + $('.un-field').val();
   });
 });
 
 app.router.add('search/:term', function(r) {
   $('#main-content').html($('#search-results').html());
-  alert(r.params.term);
 });
 
 app.router.add('institution/researchers/:id', function(r) {
   $('#main-content').html($('#manage-researchers').html());
-  alert(r.params.id);
 });
 
 app.router.add('researcher/create/:id', function(r) {
   $('#main-content').html($('#create-study').html());
-  alert(r.params.id);
+  $('.trial-start-input').pickadate();
+  $('.trial-complete-input').pickadate();
 });

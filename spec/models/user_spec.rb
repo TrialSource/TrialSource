1fcf6e04c4d88f81117c3612dcbbbb6ec5b21adf
Ashley Ellis <anellis12@gmail.com>
2015-03-16 11:55:23 -0400

    Pass email validation test and add password validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,11 @@ require_relative '../spec_helper.rb'
 
 describe User do
 	it "is invalid without an email" do
-		hospital = User.create()
-		expect(User.count).to eq(0)
+		 expect(User.new().valid?).to eq false
+
 	end
 
 	it "is invalid without a password" do
-		hospital = User.create(email: "maria@email.com")
-		expect(User.count).to eq(0)
+		 expect(User.new(email: "maria@email.com").valid?).to eq false
 	end
 end

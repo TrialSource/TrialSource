70a520d47d5707be301fa8ab6965969eec45c5d3
Ben Roaman <benroaman@gmail.com>
2015-03-22 09:59:06 -0400

    trial detail viewable everywhere but org trials

--- app/assets/javascripts/routes/route-organization-trials.js
+++ app/assets/javascripts/routes/route-organization-trials.js
@@ -1,3 +1,24 @@
 app.routeOrganizationTrials = function(r) {
-  $('#main-content').html($('#admin-trials').html());
-};
+  var trials = [];
+  $.getJSON('/api/v1/search', { type: 'trial', query: r.params.term }).done(function(data) {
+    $('#main-content').html($('#admin-trials').html());
+    console.log(data);
+    trials = data.searches;
+    showAllResults();
+  });
+
+  function showAllResults() {
+    var listTemplate = _.template(app.trialListing, { variable: 'm' });
+    $('.trial-list').html(listTemplate({ results: trials }));
+    addNameListeners();
+  }
+
+  function addNameListeners() {
+    $('.rslt-name').toArray().forEach(function(name, i) {
+      $(name).click(function(e) {
+        var detailTemplate = _.template(app.trialDetail.readable, { variable: 'm' });
+        $('.trial-list').html(detailTemplate({ trial: trials[i] }));
+        $('.bck-btn').click(showAllResults);
+      });
+    });
+  }};
--- app/assets/javascripts/routes/route-search-results.js
+++ app/assets/javascripts/routes/route-search-results.js
@@ -1,8 +1,25 @@
 app.routeSearchResults = function(r) {
+  var trials = [];
   $.getJSON('/api/v1/search', { type: 'trial', query: r.params.term }).done(function(data) {
     $('#main-content').html($('#search-results').html());
     console.log(data);
-    var listTemplate = _.template(app.trialListing, { variable: 'm' });
-    $('.rslts-list').html(listTemplate({ results: data.searches }));
+    trials = data.searches;
+    showAllResults();
   });
+
+  function showAllResults() {
+    var listTemplate = _.template(app.trialListing, { variable: 'm' });
+    $('.rslts-cntnr').html(listTemplate({ results: trials }));
+    addNameListeners();
+  }
+
+  function addNameListeners() {
+    $('.rslt-name').toArray().forEach(function(name, i) {
+      $(name).click(function(e) {
+        var detailTemplate = _.template(app.trialDetail.readable, { variable: 'm' });
+        $('.rslts-cntnr').html(detailTemplate({ trial: trials[i] }));
+        $('.bck-btn').click(showAllResults);
+      });
+    });
+  }
 };

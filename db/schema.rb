137291d9851c632ba9821c2ef1481122ebd130df
Maria <mliberov@gmail.com>
2015-03-22 15:10:15 -0400

    Fix destroy method in trials

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322174607) do
+ActiveRecord::Schema.define(version: 20150322154944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

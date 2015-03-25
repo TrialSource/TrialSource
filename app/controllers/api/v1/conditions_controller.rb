1eef6b57d77428d3434bd86b7391b37c96124046
anellis <anellis12@gmail.com>
2015-03-25 19:05:49 -0400

    Add number of organizations to conditions index

--- app/controllers/api/v1/conditions_controller.rb
+++ app/controllers/api/v1/conditions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ConditionsController < ApplicationController
   before_action :set_condition, only: [:update]
 
   def index
-    render json: [number_of_trials, Condition.all]
+    render json: [number_of_trials, number_of_organizations, Condition.all]
   end
 
   def create

61e9f1da3a23db7292fa1084f8b3533fa126f801
anellis <anellis12@gmail.com>
2015-03-25 21:26:16 -0400

    Delete unnecessary fuzzy finder conditions method

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -4,7 +4,7 @@ class Condition < ActiveRecord::Base
   has_and_belongs_to_many :trials
 
   def self.search(query)
-    condition = Condition.where(Condition.arel_table[:name].matches(query.downcase)) || Condition.fuzzy_search(query)
+    condition = Condition.where(Condition.arel_table[:name].matches(query.downcase))
     trials = condition.map do |c|
       c.trials
     end

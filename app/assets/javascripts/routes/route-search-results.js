2e5361f2e5de9065b89aff8ba3eb0c9b02af4057
Ben Roaman <benroaman@gmail.com>
2015-03-30 08:41:50 -0400

    added about page and removed some non-functional buttons

--- app/assets/javascripts/routes/route-search-results.js
+++ app/assets/javascripts/routes/route-search-results.js
@@ -4,13 +4,16 @@ app.routeSearchResults = function(r) {
   }
 
   var trials = [];
+  listState = [];
   $('#main-content').html($('#search-results').html());
   setHeader();
   $.getJSON(decodeURIComponent(r.params.criteria) + '&&location=&&range=').done(function(data) {
     if (data.conditions.length > 0) {
       trials = data.conditions;
+      listState = trials;
       showAllResults();
       initializeGeoform();
+      initializeGeoClear();
     } else {
       app.populateSaveMe(grabSearchTerm(), grabExclusionIds());
     }
@@ -30,15 +33,24 @@ app.routeSearchResults = function(r) {
 
       var url = decodeURIComponent(r.params.criteria) + '&&location=' + $('.zip-input').val() + '&&range=' + $('.range-input').val();
       $.getJSON(url).done(function(data) {
-        trials = data.conditions;
+        listState = data.conditions;
         showAllResults();
       });
     });
   }
 
+  function initializeGeoClear() {
+    $('.clear-geo-btn').click(function() {
+      $('.zip-input').val('');
+      $('.range-input').val('');
+      listState = trials;
+      showAllResults();
+    });
+  }
+
   function showAllResults() {
     var listTemplate = _.template(app.trialListing.search, { variable: 'm' });
-    $('.rslts-list').html(listTemplate({ results: trials }));
+    $('.rslts-list').html(listTemplate({ results: listState }));
     addNameListeners();
   }
 
@@ -46,13 +58,29 @@ app.routeSearchResults = function(r) {
     $('.rslt-name').toArray().forEach(function(name, i) {
       $(name).click(function(e) {
         var detailTemplate = _.template(app.trialDetail.search, { variable: 'm' });
-        $('.rslts-list').html(detailTemplate({ trial: trials[i] }));
+        $('.rslts-list').html(detailTemplate({ trial: listState[i] }));
         $('.bck-btn').click(showAllResults);
+        initializeRequestBtn();
         $.ajax({
           type: "POST",
-          url: '/api/v1/trials/' + trials[i].id + '/views',
+          url: '/api/v1/trials/' + listState[i].id + '/views',
         });
       });
+    });
+  }
+
+  function initializeRequestBtn() {
+    $('.more-info-btn').click(function() {
+      var email = $('.more-info-input');
+      if (!email.val().match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[A-Za-z]{2,4}/)) {
+        $('.more-info-error-message').text('invalid-input');
+        return;
+      } else {
+        $('.more-info-error-message').text('application requested!');
+        $('.more-info-error-message').css('color', 'seagreen');
+        $('.more-info-input').attr('disabled', true);
+        $('.more-info-btn').attr('disabled', true);
+      }
     });
   }
 
@@ -77,7 +105,7 @@ app.routeSearchResults = function(r) {
     var zip = $('.zip-input').val();
     var range = $('.range-input').val();
     if (!zip && !range) {
-      return true;
+      return false;
     }
     if (zip.match(/[.]/) || !Number(zip) || range.match(/[.]/) || !Number(range) || zip.length < 5) {
       return false;
